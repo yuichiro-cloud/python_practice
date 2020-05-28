a46465711d57b5dce023379e31c13b39500e00b2
yuichiro-cloud <ghjhbc1@gmail.com>
2020-05-29 01:44:39 +0900

    calculate stddev and corr.
    by using numpy

---
+++ practice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height.
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height.
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. 
+corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
+print("Correlation: " + str(corr))
