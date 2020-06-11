7c968a82560534ab18301f47bf3b6768cb7a21d7
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-11 20:57:01 +0900

    commit

--- practice.py
+++ practice.py
@@ -15,3 +15,10 @@ print("Standard Deviation: " + str(stddev))
 # Print out correlation between first and second column. 
 corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
 print("Correlation: " + str(corr))
+
+# numpyの論理演算子
+np.logical_and()
+np.logical_or()
+
+#pandas csv読み込み
+pd.read_csv(path)
